ae9d8bb143309493b915784c7c0161305815cd3e
Austin Leach <austinleach850@gmail.com>
2025-04-05 03:01:56 -0400

    collision and invincibility added

--- LudumDare57/Assets/Scripts/PlayerController.cs
+++ LudumDare57/Assets/Scripts/PlayerController.cs
@@ -1,17 +1,25 @@
+using System;
 using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
     private CharacterController controller;
     private Vector3 direction;
-    public float speed = 5.0f;
+    public float speed = 10.0f;
 
     private int lane = 1; //0 left, 1 middle, 2 right
-    public float laneDistance = 4; 
+    public float laneDistance = 4;
+
+    private int defaultLayer;
+    public string noCollisionLayerName = "no collision";
+    bool isInvincible = false;
+    float invincibleTime = 1f;
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
         controller = GetComponent<CharacterController>();
+        defaultLayer = gameObject.layer;
     }
 
     // Update is called once per frame
@@ -35,6 +43,17 @@ public class PlayerController : MonoBehaviour
             }
         }
 
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            Debug.Log("in invincible");
+            if (isInvincible == false)
+            {
+                StartCoroutine(Invincibility());
+            }
+        }
+
+        GlobalVariables.Timer(ref isInvincible, ref invincibleTime);
+
         Vector3 targetPosition = transform.position.z * transform.forward + transform.position.y * transform.up;
         if (lane == 0)
         {
@@ -44,11 +63,40 @@ public class PlayerController : MonoBehaviour
             targetPosition += Vector3.right * laneDistance;
         }
 
-        transform.position = targetPosition;
+        if (transform.position == targetPosition) {
+            return;
+        }
+        Vector3 diff = targetPosition - transform.position;
+        Vector3 moveDir = diff.normalized * 40 * Time.deltaTime;
+        if (moveDir.sqrMagnitude < diff.sqrMagnitude)
+        {
+            controller.Move(moveDir);
+        } else
+        {
+            controller.Move(diff);
+        }
+
+        
     }
 
     private void FixedUpdate()
     {
         controller.Move(direction * Time.fixedDeltaTime);
+    }
+
+    private void OnControllerColliderHit(ControllerColliderHit hit)
+    {
+        if (hit.transform.tag == "Obstacle" && !isInvincible)
+        {
+            Destroy(hit.gameObject);
+            Debug.Log("collision detected");
+        }
+    }
+
+    private System.Collections.IEnumerator Invincibility()
+    {
+        gameObject.layer = LayerMask.NameToLayer(noCollisionLayerName);
+        yield return new WaitForSeconds(invincibleTime);
+        gameObject.layer = defaultLayer;
     }
 }
